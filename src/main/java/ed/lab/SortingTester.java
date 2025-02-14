1373e3814c9dbf994b03d7f447205e8733ae5e0d
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-02-14 08:19:14 -0600

    Ejemplo 1

--- src/main/java/ed/lab/SortingTester.java
+++ src/main/java/ed/lab/SortingTester.java
@@ -31,6 +31,7 @@ public class SortingTester<T extends Comparable<T>> {
                 .average()
                 .orElse(0);
 
+
         System.out.printf("\t\tTiempo promedio: %s ms\n", average);
     }
 }
